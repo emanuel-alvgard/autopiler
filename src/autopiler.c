68c505124c85842b718b8d47b1f5c9ffac79d8b4
emanuel-alvgard <emanuel.alvgard@gmail.com>
2021-06-14 11:04:56 +0200

    -

--- src/autopiler.c
+++ src/autopiler.c
@@ -3,8 +3,6 @@
 #include "lite_io.h"
 #endif
 
-
-
 // CONFIG
 char compiler[1000] = "";
 char input_path[1000] = "";
@@ -17,21 +15,113 @@ char library_flag[10] = " -l ";
 char library_files[1000] = "";
 char run[2] = "";
 
+
+
+void config_value(int index, char *content) {
+
+    char index_string[4] = "";
+    switch (index) {
+        case 0: strcpy(index_string, "0. "); break;
+        case 1: strcpy(index_string, "1. "); break;
+        case 2: strcpy(index_string, "2. "); break;
+        case 3: strcpy(index_string, "3. "); break;
+        case 4: strcpy(index_string, "4. "); break;
+        case 5: strcpy(index_string, "5. "); break;
+    }
+    
+    int i0 = 0;
+    while (1) {
+        if (content[i0] == index_string[0]
+        && content[i0 + 1] == index_string[1]
+        && content[i0 + 1] == index_string[2]) { i0 += 3; break; }
+    }
+
+    int i1 = 0;
+    while (1) {
+        if (content[i0] == '\n' || content [i0] == '\0') { return; }
+        switch (index) {
+            case 0: compiler[i1]; break;
+            case 1: input_file[i1]; break;
+            case 2: output_path[i1]; break;
+            case 3: output_file[i1]; break;
+            case 4: object_files[i1]; break;
+            case 5: library_files[i1]; break;
+        }
+        i0 += 1;
+        i1 += 1;
+    }
+}
+
+
+
+int config_file() {
+    
+    char _config_file[100] = "";
+    int count = 0;
+    
+    while (1) {
+        int error = directory_index(count, input_path, _config_file);
+        count += 1;
+        
+        if (strcmp(_config_file, "autopiler.txt") == 0) { break; }
+        if (error == 1) { return 1;}    
+    }
+
+    char user_input[10] = "";
+    printf("Use config file? (y/n):\n> ");
+    scanf("%s", user_input);
+    
+    while (1) {
+        if (strcmp(user_input, "n") == 0) { return 1; }
+        if (strcmp(user_input, "y") == 0) { break; }
+
+        printf("Invalid input. Please enter 'y' or 'n':\n>");
+        scanf("%s", user_input);
+    }
+
+    strcpy(_config_file, "/autopiler.txt");
+    char config_file_path[1000] = "";
+    strcat(config_file_path, input_path);
+    strcat(config_file_path, _config_file);
+    
+    long config_file_size = 0;
+    file_size(config_file_path, &config_file_size);
+    
+    char *content = calloc(config_file_size, 1);
+    file_read(config_file_path, content);
+
+    config_value(0, content); // compiler
+    config_value(1, content); // input file
+    config_value(2, content); // output path
+    config_value(3, content); // output file
+    config_value(4, content); // object files
+    config_value(5, content); // library files
+
+    return 0;
+}
+
+
+
 int main() {
     
     printf("Autopiler v.1.0.0\n");
     printf("Please enter input directory path:\n> ");
     scanf("%s", input_path);
-    printf("Please enter input file name:\n> ");
-    scanf("%s", input_file);
-    printf("Please enter ouput directory path:\n> ");
-    scanf("%s", output_path);
-    printf("Please enter ouput file name:\n> ");
-    scanf("%s", output_file);
-    printf("Please enter library files:\n> ");
-    scanf("%s", library_files);
 
-    strcat(compiler, "gcc ");
+    // look for autopiler.txt config file
+    if (config_file() != 0) {
+
+        strcat(compiler, "gcc ");
+        printf("Please enter input file name:\n> ");
+        scanf("%s", input_file);
+        printf("Please enter ouput directory path:\n> ");
+        scanf("%s", output_path);
+        printf("Please enter ouput file name:\n> ");
+        scanf("%s", output_file);
+        printf("Please enter library files:\n> ");
+        scanf("%s", library_files);
+    }
+
     strcat(input_path, "/");
     strcat(output_path, "/");
 
@@ -39,11 +129,15 @@ int main() {
     strcat(result, compiler);
     strcat(result, input_path);
     strcat(result, input_file);
-    strcat(result, output_flag);
-    strcat(result, output_path);
-    strcat(result, output_file);
-    strcat(result, library_flag);
-    strcat(result, library_files);
+    if (output_path != "" || input_file != "") {
+        strcat(result, output_flag);
+        strcat(result, output_path);
+        strcat(result, output_file);
+    }
+    if (library_files != "") {
+        strcat(result, library_flag);
+        strcat(result, library_files);
+    }
 
     char scan_path[1000] = "";
     strcat(scan_path, input_path);
